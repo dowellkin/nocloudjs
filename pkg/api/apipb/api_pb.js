6051c90363015656347254ca63ac0a8d45f31c44
Alexandr Korolev <dowellkin@gmail.com>
2021-11-18 14:53:39 +0300

    first commit

---
+++ pkg/api/apipb/api_pb.js
@@ -0,0 +1,33 @@
+// source: pkg/api/apipb/api.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+var jspb = require('google-protobuf');
+var goog = jspb;
+var global = (function() {
+  if (this) { return this; }
+  if (typeof window !== 'undefined') { return window; }
+  if (typeof global !== 'undefined') { return global; }
+  if (typeof self !== 'undefined') { return self; }
+  return Function('return this')();
+}.call(null));
+
+var pkg_google_api_annotations_pb = require('../../../pkg/google/api/annotations_pb.js');
+goog.object.extend(proto, pkg_google_api_annotations_pb);
+var pkg_accounting_accountspb_accounts_pb = require('../../../pkg/accounting/accountspb/accounts_pb.js');
+goog.object.extend(proto, pkg_accounting_accountspb_accounts_pb);
+var pkg_accounting_namespacespb_namespaces_pb = require('../../../pkg/accounting/namespacespb/namespaces_pb.js');
+goog.object.extend(proto, pkg_accounting_namespacespb_namespaces_pb);
+var pkg_services_proto_services_pb = require('../../../pkg/services/proto/services_pb.js');
+goog.object.extend(proto, pkg_services_proto_services_pb);
+var pkg_health_healthpb_health_pb = require('../../../pkg/health/healthpb/health_pb.js');
+goog.object.extend(proto, pkg_health_healthpb_health_pb);
